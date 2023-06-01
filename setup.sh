a0aee9708a0438a347e926143d805079df03762c
Laroy Sjödahl <lsjodahl@verifa.io>
2023-06-01 15:22:47 +0200

    Removes chmod

--- setup.sh
+++ setup.sh
@@ -6,13 +6,8 @@ docker-compose up -d --build
 
 # add to hosts
 docker exec ansible_katas_workspace cat ../root/.ssh/id_rsa.pub > id_rsa.pub
-
-# some windows specific msys stuff for paths(?) MSYS_NO_PATHCONV=1
-
 docker exec ansible_katas_host_1 mkdir root/.ssh
 docker cp id_rsa.pub ansible_katas_host_1:root/.ssh/authorized_keys
-docker exec ansible_katas_host_1 chmod 700 root/.ssh
-docker exec ansible_katas_host_1 chmod 600 root/.ssh/authorized_keys
 
 rm id_rsa.pub
 
