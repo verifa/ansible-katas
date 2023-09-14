b228fc412f22ed5268dfa0ad365be25a6c494d49
spektrum1 <viktor.altintas@gmail.com>
2023-09-14 10:05:52 +0200

    move molecule into its own exercise

---
+++ 10-testing-with-molecule/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kata="$(basename $(pwd))"
+
+#for docker to know where to mount to
+echo "EXERCISE_PATH=$(pwd)" > ../.utils/.env
+
+cd ../.utils
+. setup.sh $kata
+cd - > /dev/null
+
